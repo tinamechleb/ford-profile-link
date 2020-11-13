36e25d94f2f91fd06c73d8e001b5220231f7d358
Tina <tinamechleb@gmail.com>
2020-11-13 11:02:42 +0200

    fetching data from cms

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,49 +16,24 @@
     </head>
     <body class="antialiased m-0">
         <div style="position: absolute;top: 5%;left:0;z-index: 10;flex-direction: column;" class="d-grid justify-content-center w-100">
-            <img src="ford-logo.png" class="w-100" style="max-width: 150px;justify-self:center;"/>
-            <p class="text-align-center my-4 blue-text font-weight-bold">@fordmynaghi</p>
+            <img src="{{$settings->logo}}" class="w-100" style="max-width: 150px;justify-self:center;"/>
+            <p class="text-align-center my-4 blue-text font-weight-bold">{{$settings->username}}</p>
             <div style="margin-bottom: 100px;">
-                <div class="link-container">
-                    <p>Official Website</p>
-                </div>
-                <div class="link-container">
-                    <p>Official Website</p>
-                </div>
-                <div class="link-container">
-                    <p>Official Website</p>
-                </div>
-                <div class="link-container">
-                    <p>Official Website</p>
-                </div>
-                <div class="link-container">
-                    <p>Official Website</p>
-                </div>
-                <div class="link-container">
-                    <p>Official Website</p>
-                </div>
-                <div class="link-container">
-                    <p>Official Website</p>
-                </div>
-                <div class="link-container">
-                    <p>Official Website</p>
-                </div>
-                <div class="link-container">
-                    <p>Official Website</p>
-                </div>
-                <div class="link-container">
-                    <p>Official Website</p>
-                </div>
+                @foreach($links as $link)
+                <a href="{{$link->link}}" class="link-container">
+                    <p>{{$link->title}}</p>
+                </a>
+                @endforeach
             </div>
         </div>
         <div class="footer-container">
-            <img src="footer.png"/>
+            <img src="{{$settings->footer_image}}"/>
         </div>
             <div> 
-            <div class="slider-container" style="z-index: 9; position: fixed; height: 100%">
-                <div><img src="{{asset('background-01.png')}}" style="height: 100vh; width: 100vw;object-fit: cover;"/></div>
-                <div><img src="{{asset('background-01.png')}}" style="height: 100vh; width: 100vw;object-fit: cover;"/></div>
-                <div><img src="{{asset('background-01.png')}}" style="height: 100vh; width: 100vw;object-fit: cover;"/></div>
+            <div class="slider-container" style="z-index: 9; position: relative; height: 100%">
+                @foreach($slider as $image)
+                    <div><img src="{{$image->image}}" style="height: 100vh; width: 100vw;object-fit: cover;"/></div>
+                @endforeach
             </div>
 </div>
         <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    $settings = DB::table('settings')->first();
+    $slider = DB::table('slider')->get();
+    $links = DB::table('links')->get();
+    return view('index', ['settings' => $settings, 'slider' => $slider, 'links' => $links]);
 });
 
 
